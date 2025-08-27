0dc70f3677cd3f6ae2487114f3d8bdb06ed3a0a2
Phong <thieu.phong-le95@gmail.com>
2025-08-27 14:54:49 +0200

    ones somehow works

--- jatzy.test.js
+++ jatzy.test.js
@@ -1,37 +1,22 @@
 import { test, expect } from "vitest";
 
-function yatzeeScore(category, numbers) {
+function checkDie(category, numbersArray) {
   let result = 0;
-  if (category === "Ones") {
-    for (const die of numbers) {
-      if (die == 1) {
-        result += 1;
+
+  function dieRoll(category, number) {
+    for (const die of numbersArray) {
+      if (die == number) {
+        result += number;
       }
     }
-    return result;
   }
+  dieRoll("Ones", 1);
 
-  if (category === "Twos") {
-    for (const die of numbers) {
-      if (die == 2) {
-        result += 2;
-      }
-    }
-    return result;
-  }
-
-  for (const die of numbers) {
-    result += die;
-  }
   return result;
 }
 
-function checkDie(category, die, numbers) {}
-
 test("scoring Chance add all dice", () => {
-  expect(yatzeeScore("Chance", [6, 6, 6, 6, 6])).toBe(30);
-  expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1 + 2 + 3 + 4 + 5);
-  expect(yatzeeScore("Ones", [1, 1, 3, 4, 5])).toBe(2);
-  expect(yatzeeScore("Ones", [1, 1, 1, 1, 1])).toBe(5);
-  expect(yatzeeScore("Twos", [2, 2, 1, 1, 1])).toBe(4);
+  expect(checkDie("Ones", [1, 1, 3, 4, 5])).toBe(2);
+  expect(checkDie("Ones", [1, 1, 1, 1, 1])).toBe(5);
+  expect(checkDie("Ones", [1, 5, 6, 1, 1])).toBe(3);
 });
