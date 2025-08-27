b10c7ee774a07371bdb0603bc0a6857db2983e56
Shaygan Kordestani <shaygan1994@gmail.com>
2025-08-27 14:00:48 +0200

    husky

--- jatzy.test.js
+++ jatzy.test.js
@@ -1,7 +1,11 @@
 import { test, expect } from "vitest";
 
 function yatzeeScore(chance, numbers) {
-  return 1 + 2 + 3 + 4 + 5;
+  let result = 0;
+  for (const die of numbers) {
+    result += die;
+  }
+  return result;
 }
 
 test("scoring Chance add all dice", () => {
