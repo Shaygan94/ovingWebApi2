560940bfc9f148fdd22a1a84ff9ef623990f627e
Phong <thieu.phong-le95@gmail.com>
2025-08-27 15:13:33 +0200

    It somehow works

--- jatzy.test.js
+++ jatzy.test.js
@@ -3,13 +3,16 @@ import { test, expect } from "vitest";
 function checkDie(category, numbersArray) {
   let result = 0;
 
-  function dieRoll(category, number) {
-    for (const die of numbersArray) {
-      if (die === number) {
-        result += number;
+  function dieRoll(dieCategory, number) {
+    if (dieCategory === category) {
+      for (const die of numbersArray) {
+        if (die === number) {
+          result += number;
+        }
       }
     }
   }
+  dieRoll("Twos", 2);
   dieRoll("Ones", 1);
 
   return result;
@@ -19,4 +22,5 @@ test("scoring Chance add all dice", () => {
   expect(checkDie("Ones", [1, 1, 3, 4, 5])).toBe(2);
   expect(checkDie("Ones", [1, 1, 1, 1, 1])).toBe(5);
   expect(checkDie("Ones", [1, 5, 6, 1, 1])).toBe(3);
+  expect(checkDie("Twos", [2, 2, 6, 1, 1])).toBe(4);
 });
