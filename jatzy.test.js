bc1920349cc062ba536ebd1371e8840aa183815b
Phong <thieu.phong-le95@gmail.com>
2025-08-27 15:19:26 +0200

    It somehow works

--- jatzy.test.js
+++ jatzy.test.js
@@ -22,5 +22,5 @@ test("scoring Chance add all dice", () => {
   expect(checkDie("Ones", [1, 1, 3, 4, 5])).toBe(2);
   expect(checkDie("Ones", [1, 1, 1, 1, 1])).toBe(5);
   expect(checkDie("Ones", [1, 5, 6, 1, 1])).toBe(3);
-  expect(checkDie("Twos", [2, 2, 6, 1, 1])).toBe(4);
+  expect(checkDie("Twos", [2, 2, 6, 1, 1])).toBe(4); //hehe
 });
