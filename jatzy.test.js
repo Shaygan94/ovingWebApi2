137ee83fadf22e758378b096188cb3446f048118
kevph <kevphan073@gmail.com>
2025-08-27 13:56:42 +0200

    Made it work!

--- jatzy.test.js
+++ jatzy.test.js
@@ -1,9 +1,10 @@
 import { test, expect } from "vitest";
 
 function yatzeeScore(chance, numbers) {
-    return 1+2+3+4+5;
+  return 1 + 2 + 3 + 4 + 5;
 }
 
 test("scoring Chance add all dice", () => {
+  expect(yatzeeScore("Chance", [6, 6, 6, 6, 6])).toBe(6 + 6 + 6 + 6 + 6);
   expect(yatzeeScore("Chance", [1, 2, 3, 4, 5])).toBe(1 + 2 + 3 + 4 + 5);
 });
